20298014d97a2f0c644d227db458678b1cd9903e
Ubuntu <ubuntu@ip-172-31-9-216.ap-northeast-1.compute.internal>
2023-01-20 02:14:38 +0000

    update_2023

--- src/migration/1612923944843-seedConfigAndAdminUser.ts
+++ src/migration/1612923944843-seedConfigAndAdminUser.ts
@@ -7,7 +7,7 @@ export class seedConfigAndAdminUser1612923944843 implements MigrationInterface {
         await queryRunner.query("insert into user(name, email, role, createdAt, password) " + 
                                 "values ('Ho Van Vy', 'hovanvydut@gmail.com', 'ADMIN', now(), '$2b$10$Ej957EhTnZrKdQqs3e4speoxq2bOVphYCAuvpZLA72KM/R69xEEK2')");
 
-        await queryRunner.query("insert into gift(value, max_quantity) values (20, 1), (10, 3), (5, 6), (2, 10), (0, 2147483647)");
+        await queryRunner.query("insert into gift(value, max_quantity) values (20, 3), (10, 9), (5, 18), (2, 30), (0, 2147483647)");
 
         await queryRunner.query(`insert into config_app (my_key, my_value, data_type) values("WIN_RATE", "8.0", "float"), ("MAX_DRAW_EACH_PERSON", "10", "int")`);
 
